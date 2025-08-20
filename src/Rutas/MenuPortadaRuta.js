692d0fcf053db2ac141cc8ae04fa29e58d0b5ce0
prosystem-pro <prosystem490@gmail.com>
2025-08-20 07:39:17 -0600

    Se implementó el apartado de subir imagen en Menu Portada

--- src/Rutas/MenuPortadaRuta.js
+++ src/Rutas/MenuPortadaRuta.js
@@ -3,8 +3,11 @@ const Router = Express.Router();
 const Modelo = 'menuPortada';
 const Tabla = 'MenuPortada'
 const { Listado, ObtenerPorCodigo, Buscar, Crear, Editar, Eliminar } = require('../Controladores/MenuPortadaControlador');
+const { SubirImagen } = require("../Controladores/SubirImagenControlador");
+
 const VerificarToken = require('../FuncionIntermedia/VerificarToken');
-const VerificarPermisos = require('../FuncionIntermedia/VerificarPermisos'); 
+const VerificarPermisos = require('../FuncionIntermedia/VerificarPermisos');
+const { Subir } = require("../FuncionIntermedia/SubirImagen"); 
 
 Router.get(`/${Modelo}/listado`, Listado);
 Router.get(`/${Modelo}/:Codigo`,VerificarToken,VerificarPermisos('VerUnidad',Tabla), ObtenerPorCodigo);
@@ -12,5 +15,6 @@ Router.get(`/${Modelo}/buscar/:TipoBusqueda/:ValorBusqueda`,VerificarToken,Verif
 Router.post(`/${Modelo}/crear`, VerificarToken,VerificarPermisos('Crear',Tabla),Crear);
 Router.put(`/${Modelo}/editar/:Codigo`, VerificarToken,VerificarPermisos('Editar',Tabla), Editar);
 Router.delete(`/${Modelo}/eliminar/:Codigo`, VerificarToken,VerificarPermisos('Eliminar',Tabla),  Eliminar);
+Router.post(`/${Modelo}/subir-imagen`, VerificarToken, VerificarPermisos("SubirImagen", Tabla),Subir.single("Imagen"), SubirImagen);
 
 module.exports = Router;
