9e3452a35b5d69f2f69931d6a611c3b85a82903f
prosystem-pro <prosystem490@gmail.com>
2025-08-21 17:29:16 -0600

    Subir imagen implementada en Pago

--- src/Rutas/PagoRuta.js
+++ src/Rutas/PagoRuta.js
@@ -3,8 +3,11 @@ const Router = Express.Router();
 const Modelo = 'pago';
 const Tabla = 'Pago'
 const { Listado, ObtenerPorCodigo, Buscar, Crear, Editar, Eliminar } = require('../Controladores/PagoControlador');
+const { SubirImagen } = require("../Controladores/SubirImagenControlador");
+
 const VerificarToken = require('../FuncionIntermedia/VerificarToken');
 const VerificarPermisos = require('../FuncionIntermedia/VerificarPermisos'); 
+const { Subir } = require("../FuncionIntermedia/SubirImagen");
 
 Router.get(`/${Modelo}/listado/:anio`,VerificarToken,VerificarPermisos('Listado',Tabla), Listado);
 Router.get(`/${Modelo}/:Codigo`,VerificarToken,VerificarPermisos('VerUnidad',Tabla), ObtenerPorCodigo);
@@ -12,5 +15,7 @@ Router.get(`/${Modelo}/buscar/:TipoBusqueda/:ValorBusqueda`,VerificarToken,Verif
 Router.post(`/${Modelo}/crear`,VerificarToken,VerificarPermisos('Crear',Tabla), Crear);
 Router.put(`/${Modelo}/editar/:Codigo`,VerificarToken,VerificarPermisos('Editar',Tabla), Editar);
 Router.delete(`/${Modelo}/eliminar/:Codigo`,VerificarToken,VerificarPermisos('Eliminar',Tabla), Eliminar);
+Router.post(`/${Modelo}/subir-imagen`, VerificarToken, VerificarPermisos("SubirImagen", Tabla),Subir.single("Imagen"), SubirImagen);
+
 
 module.exports = Router;
